7aa557cc0ef8d5830f913ea4ad144bf363482ff5
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-08-12 19:10:31 -0500

    bottombar for homepage done

--- frontend/components/shared/Bottombar.tsx
+++ frontend/components/shared/Bottombar.tsx
@@ -1,5 +1,43 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import Image from "next/image"
+import { sidebarLinks } from "@/constants"
+
 const Bottombar = () => {
-	return <h1>Bottombar</h1>
+	const pathname = usePathname()
+
+	return (
+		<section className="bottombar">
+			<div className="bottombar_container">
+				{sidebarLinks.map(link => {
+					const isActive =
+						(pathname.includes(link.route) &&
+							link.route.length > 1) ||
+						pathname === link.route
+					return (
+						<Link
+							href={link.route}
+							key={link.label}
+							className={`bottombar_link ${
+								isActive && "bg-primary-500"
+							}`}>
+							<Image
+								src={link.imgURL}
+								alt={link.label}
+								width={24}
+								height={24}
+							/>
+							<p className="text-light-1 text-subtle-medium max-sm:hidden">
+								{link.label.split(/\s+./)[0]}
+							</p>
+						</Link>
+					)
+				})}
+			</div>
+		</section>
+	)
 }
 
 export default Bottombar
